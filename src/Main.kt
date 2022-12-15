0c86d0594211df0871f2dcb1705665fee7c44fc8
Rodrigo Pinto <dclankpt9@gmail.com>
2022-12-15 11:29:58 +0000

    Second commit FichaFuncional2.iml

--- src/Main.kt
+++ src/Main.kt
@@ -17,7 +17,7 @@ fun getNomesDosAlunosAprovados(lista: List<Aluno>): String{
 }
 
 fun quantasNotasDiferentes(lista: List<Aluno>): Int{
-    return lista.map{ it.nome }
+    return lista.map{ it.nota }
             .distinct()
             .count()
 }
@@ -29,10 +29,19 @@ fun mediaNotasEstrangeiras(lista: List<Aluno>): Double{
 }
 
 fun nomeDos2AlunosComNotaMaisAltaOrdenadasDecrescente(lista: List<Aluno>): List<String>{
+    return lista.sortedWith{n1,n2 -> n2.nota - n1.nota}
+            .map { it.nome }
+            .take(2)
+}
 
+fun dadosDos3AlunosComNotaMaisAltaOrdenadosCrescente(lista: List<Aluno>): List<String>{
+    return lista.sortedWith{n1,n2 -> n1.nota - n2.nota}
+            .map { it.nome + " : " + it.nacionalidade + " : " + it.nota   }
+            .takeLast(3)
 }
 
 fun main(){
+    /*
     val listaTeste  = mutableListOf<Aluno>()
     listaTeste.add(Aluno("Rodrigo",12,"Portuguesa"))
     listaTeste.add(Aluno("Bernardo",8,"Portuguesa"))
@@ -43,6 +52,8 @@ fun main(){
     println(getNomesDosAlunosAprovados(listaTeste))
     println(quantasNotasDiferentes(listaTeste))
     println(mediaNotasEstrangeiras(listaTeste))
-
-
+    println(nomeDos2AlunosComNotaMaisAltaOrdenadasDecrescente(listaTeste))
+    println(dadosDos3AlunosComNotaMaisAltaOrdenadosCrescente(listaTeste))
+    
+     */
 }
